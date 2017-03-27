9bc9119d07188a9d76acf66bbb995548f8752fec
Deshi Xiao <xiaods@gmail.com>
2017-03-28 00:49:12 +0800

    FIX: correct stout/ns.hpp path.

--- src/slave/containerizer/mesos/isolators/namespaces/ipc.cpp
+++ src/slave/containerizer/mesos/isolators/namespaces/ipc.cpp
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "linux/ns.hpp"
+#include "stout/ns.hpp"
 
 #include "slave/containerizer/mesos/isolators/namespaces/ipc.hpp"
 
